c19a9065a7bc38f7d4fb61d6ad90f79a74bb3896
Etukuri Bharath <etukuri.bharath123@gmai.com>
2020-08-30 17:37:36 +0530

    Static tooltip implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
+  const [inElem,setInElem]=useState(null);
+  useEffect(() => {
+    let b = document.getElementById('tooltip1');
+    setInElem(b);
+  },[]);
+  function show (elem1,elem2) {
+    let a = document.getElementById(elem1);
+    let b = document.getElementById(elem2);
+    b.style.display="block";
+    a.style.display="";
+  }
+
+  // let b = document.getElementById('tooltip1');
+  // b?b.style.display="block":null;
+
   return (
-    <div className="App">
+    <div className="App1">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Tooltip Player
       </header>
+
+      <div className="name" onClick={()=>{show('tooltip1', 'tooltip2')}}>
+        NAME
+        <div className="tooltip" id="tooltip1">
+          PROFILE DETAILS
+        </div>
+      </div>
+
+      <div className="name" onClick={()=>{show('tooltip2', 'tooltip3')}}>
+        NAME 2
+        <div className="tooltip" id="tooltip2">
+          PROFILE DETAILS 2
+        </div>
+      </div>
+
+      <div className="name" onClick={()=>{show('tooltip3', 'tooltip3')}}>
+        NAME 3
+        <div className="tooltip" id="tooltip3">
+          PROFILE DETAILS 3
+        </div>
+      </div>
+      { inElem?inElem.style.display="block":null}
     </div>
   );
 }
